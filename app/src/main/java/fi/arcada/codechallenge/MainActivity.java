3a4ff555eba8428d73c2694c08a0d64e93c90385
kuvajaan <kuvajaan@gmail.com>
2025-03-25 10:11:19 +0200

    Update Gradle

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,8 @@
 package fi.arcada.codechallenge;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView myview;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myview = findViewById(R.id.MyView);
+
+        myview.setText("My first app");
+
+
 
     }
 }
